d3097f9ef6232a46dafb83140770672b993aa453
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-01-14 15:59:28 +0100

    Image not delete just marked as viewed

--- src/components/List/Single/Single.js
+++ src/components/List/Single/Single.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { ListItem, Thumbnail, Text } from 'native-base';
+import { ListItem, Thumbnail, Text, Icon } from 'native-base';
 
 export default class Single extends Component {
+  checkAction() {
+    if (this.props.snap.viewed) return;
+    this.props.itemPressed(this.props.snap.id);
+  }
+
+  iconToDisplay() {
+    return this.props.snap.viewed ? 'ios-checkmark-outline' : 'ios-cloud-download-outline';
+  }
+
+  componentDidMount() {
+    console.log(this.props.snap);
+  }
+
   render() {
     return (
-      <ListItem onPress={this.props.itemPressed.bind(null, this.props.snap.id)}>
+      <ListItem onPress={this.checkAction.bind(this)}>
         <Thumbnail size={40} source={{uri: this.props.snap.user.image}} />
         <Text>{this.props.snap.user.email}</Text>
         <Text note>Reçu le {this.props.snap.created_at}</Text>
+        <Icon name={this.iconToDisplay()} style={{position: 'absolute', right: 10}}/>
       </ListItem>
     );
   }

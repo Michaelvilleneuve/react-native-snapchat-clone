2aecba753962a27257e3391322294b75e5b615e7
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-01-14 21:20:35 +0100

    Add authentication, fixed #2 🙃

--- models/User.js
+++ models/User.js
@@ -1,8 +1,31 @@
 import API from '../config.js';
 
 const User = {
+  isConnected: false,
   findAll() {
-    return fetch(`${API.url}users`).then(response => response.json())
+    return fetch(`${API.url}users`, {
+    	headers: API.headers
+    }).then(response => response.json())
+  },
+  isConnected() {
+  	return false;
+  },
+  connect(content) {
+  	const data = JSON.stringify(content);
+
+  	return fetch(`${API.url}auth/sign_in`, {
+    	method: 'post',
+    	body: data
+    }).then(response => {
+    	if(response.status === 200) {
+    		console.log(response)
+	      	API.headers['access-token'] = response.headers.map['access-token'][0];
+	      	API.headers['client'] = response.headers.map['client'][0];
+	      	API.headers['uid'] = response.headers.map['uid'][0];
+	    	User.isConnected = true;
+	    }
+    	return response;
+    })
   }
 }
 

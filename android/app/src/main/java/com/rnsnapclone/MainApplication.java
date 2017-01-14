1369ed9faa012fd658f95d497c5fcce85153b4ac
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-01-14 21:16:59 +0100

    Update android and ios build

--- android/app/src/main/java/com/rnsnapclone/MainApplication.java
+++ android/app/src/main/java/com/rnsnapclone/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.github.xfumihiro.react_native_image_to_base64.ImageToBase64Package;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImageToBase64Package(),
             new RCTCameraPackage(),
             new VectorIconsPackage()
       );

361a83d31e2f4dfc1c47239b3c404d64cfceed74
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-01-14 21:17:17 +0100

    Add headers to API

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-const API = {
-	url: "http://192.168.1.28:3000/"
+let API = {
+  url: "http://192.168.1.28:3000/",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 }
 
 export default API;
